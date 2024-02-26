518f1e0a2c51b83660ec8ad809036911ee357348
cqlyj <luoyingjie0721@gmail.com>
2024-02-26 16:35:58 +0800

    complete deposit

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,12 +1,31 @@
 const { getNamedAccounts } = require("hardhat");
-const { getWeth } = require("./getWeth");
+const { getWeth, AMOUNT } = require("./getWeth");
 
 async function main() {
   await getWeth();
   const { deployer } = await getNamedAccounts();
   const signer = await ethers.provider.getSigner();
+
+  // get LendingPool
   const lendingPool = await getLendingPool(signer);
   console.log(`LendingPool address ${lendingPool.target}`);
+
+  // deposit
+  const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  await approveErc20(wethTokenAddress, lendingPool.target, AMOUNT, signer);
+  console.log("Depositing....");
+  await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
+  console.log("Desposited!");
+
+  // borrow
+  
+}
+
+async function approveErc20(erc20Address, spenderAddress, amount, signer) {
+  const erc20Token = await ethers.getContractAt("IERC20", erc20Address, signer);
+  txResponse = await erc20Token.approve(spenderAddress, amount);
+  await txResponse.wait(1);
+  console.log("Approved!");
 }
 
 async function getLendingPool(account) {
--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -4,11 +4,11 @@ const AMOUNT = ethers.parseEther("0.02");
 
 async function getWeth() {
   const { deployer } = await getNamedAccounts();
-  const signer = await ethers.provider.getSigner()
+  const signer = await ethers.provider.getSigner();
   const iWeth = await ethers.getContractAt(
     "IWeth",
     "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    signer,
+    signer
   );
 
   const tx = await iWeth.deposit({ value: AMOUNT });
@@ -17,4 +17,4 @@ async function getWeth() {
   console.log(`get ${wethBalance} weth!`);
 }
 
-module.exports = { getWeth };
+module.exports = { getWeth, AMOUNT };
