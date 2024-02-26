5ce3284cbf00a1e106fde5ba7416338735a3d395
cqlyj <luoyingjie0721@gmail.com>
2024-02-26 14:41:28 +0800

    get LendingPool address

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,7 +1,28 @@
+const { getNamedAccounts } = require("hardhat");
 const { getWeth } = require("./getWeth");
 
 async function main() {
   await getWeth();
+  const { deployer } = await getNamedAccounts();
+  const signer = await ethers.provider.getSigner();
+  const lendingPool = await getLendingPool(signer);
+  console.log(`LendingPool address ${lendingPool.target}`);
+}
+
+async function getLendingPool(account) {
+  const lendingPoolAddressesProvider = await ethers.getContractAt(
+    "ILendingPoolAddressesProvider",
+    "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+    account
+  );
+  const lendingPoolAddress =
+    await lendingPoolAddressesProvider.getLendingPool();
+  const lendingPool = await ethers.getContractAt(
+    "ILendingPool",
+    lendingPoolAddress,
+    account
+  );
+  return lendingPool;
 }
 
 main()
